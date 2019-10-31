c082a9945b64c81b5513713b7f6c6f01411fd574
Gareth Johns <garethjohns@Gareths-iMac.local>
2019-10-31 13:56:13 +0000

    Initial Commit

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -12,8 +12,8 @@ import Swiper from 'swiper';
 import Cookies from 'js-cookie'
 
 
-  // Foundation
-  // ----------
+// 2. Foundation
+// ----------
   
 Foundation.Interchange.SPECIAL_QUERIES['medium-retina'] = 'only screen and (min-width: 40em), (min-width: 40em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 40em) and (min--moz-device-pixel-ratio: 2), (min-width: 40em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 40em) and (min-device-pixel-ratio: 2), (min-width: 40em) and (min-resolution: 192dpi), (min-width: 40em) and (min-resolution: 2dppx)';
 Foundation.Interchange.SPECIAL_QUERIES['large-retina'] = 'only screen and (min-width: 64em), (min-width: 64em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 64em) and (min--moz-device-pixel-ratio: 2), (min-width: 64em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 64em) and (min-device-pixel-ratio: 2), (min-width: 64em) and (min-resolution: 192dpi), (min-width: 64em) and (min-resolution: 2dppx)';
@@ -23,7 +23,101 @@ Foundation.Interchange.SPECIAL_QUERIES['xxlarge-retina'] = 'only screen and (min
 $(document).foundation();
 
 
-// 2. Animate on Scroll
+// 3. Loading
+// ----------
+
+$(function() {
+
+
+    $(".loader").removeClass("hide");
+
+var singleNumber = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+var tenPlus = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+var tens = ['twenty', 'thirty', 'forty', 'fifty'];
+
+function num2word(str) {
+    str = str.toString().replace(/[\, ]/g, '');
+
+    if (str != parseFloat(str)) {
+        return 'not a number';
+    }
+
+    var strLength = str.indexOf('.');
+    if (strLength == -1) {
+        strLength = str.length;
+    }
+
+    var n = str.split('');
+    var result = '';
+    var skip = 0;
+    for (var i = 0; i < strLength; i++) {
+
+        if ((strLength - i) % 3 == 2) {
+            if (n[i] == '1') {
+                result += tenPlus[Number(n[i + 1])] + ' ';
+                i++;
+                skip = 1;
+            } else if (n[i] != 0) {
+                result += tens[n[i] - 2] + ' ';
+                skip = 1;
+            }
+        } else if (n[i] != 0) {
+            result += singleNumber[n[i]] + ' ';
+        }
+    }
+
+    return result.replace(/\s+/g, ' ');
+}
+
+/* 
+Selectors 
+*/
+var hour = document.getElementsByClassName('hour'),
+    min = document.getElementsByClassName('min'),
+    sec = document.getElementsByClassName('sec');
+//     ampm = document.getElementsByClassName('ampm');
+
+var firstTime = true;
+
+/*
+Updated the clock 
+*/
+function updateClock(d) {
+  var hours = ((d.getHours() + 11) % 12 + 1),
+      minutes = d.getMinutes(),
+      seconds = d.getSeconds();
+//       period = (d.getHours() < 12) ? "am" : "pm";
+  
+  console.log(hours);
+   
+  hour[0].innerHTML = num2word(hours);
+  min[0].innerHTML = num2word(minutes) + '&';
+  sec[0].innerHTML = num2word(seconds) + 'seconds';
+//   ampm[0].innerHTML = period;
+  
+}
+
+/*
+Run updateClock every second 
+*/
+setInterval(function() {
+  var d = new Date();
+  updateClock(d);
+}, 1000);
+
+
+    $(".loader").addClass("loading");
+
+    setTimeout(function(){
+     $(".loader").addClass("loaded");
+    }, 5000); 
+
+});
+
+
+// 4. Animate on Scroll
 // --------------------
 
 $(function() {
@@ -38,6 +132,9 @@ $(function() {
 window.addEventListener('load', AOS.refresh);
 });
 
+
+// 5. Carousel
+// -----------
 
 $(document).ready(function () {
 
@@ -67,6 +164,8 @@ $(document).ready(function () {
     })
   });
 
+// 6. Lightgallery
+// ---------------
 
 $('.video').lightGallery({
     counter: false,
